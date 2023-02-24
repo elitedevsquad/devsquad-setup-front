b22cd7f45269581653e35b5fc9a05ef1df5c87e7
Matheus Santos <matheussantos@devsquad.com>
2023-02-24 11:04:43 -0300

    Added option to install eslint config file for nuxt projects

---
+++ verification-tools/.eslintrc-nuxt.js
@@ -0,0 +1,20 @@
+// eslint-ignore-next-line no-undef
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        '@nuxtjs/eslint-config-typescript',
+        'eslint:recommended',
+        'plugin:vue/recommended',
+        'plugin:nuxt/recommended',
+        'prettier'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {}
+}
