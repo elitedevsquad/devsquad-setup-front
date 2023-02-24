2d257bba12ce07dbe40bb16fcc626dffd88814f2
Matheus Santos <matheussantos@devsquad.com>
2023-02-24 09:54:03 -0300

    Added eslint config file

---
+++ verification-tools/.eslintrc.js
@@ -0,0 +1,18 @@
+// eslint-ignore-next-line no-undef
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'eslint:recommended',
+        'prettier'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {
+    }
+}
