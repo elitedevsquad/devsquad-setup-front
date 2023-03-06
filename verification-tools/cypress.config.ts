f2f7ff9cbd06276d51b4f5231867d604683e573c
Matheus Santos <matheussantos@devsquad.com>
2023-03-06 08:40:04 -0300

    Removed `setupNodeEvents` method from `e2e` config key

--- verification-tools/cypress.config.ts
+++ verification-tools/cypress.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
     video: false,
     e2e: {
-        baseUrl: 'http://localhost:3000',
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
+        baseUrl: 'http://localhost:3000'
     }
 })
